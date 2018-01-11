e7e9aa2260c9f89349afb875a5398df140dfa3f5
Mark Gregan <mark@gregan.org>
2018-01-10 20:52:51 -0500

    Adding model, controller, and migration for Tickets

---
+++ database/migrations/2018_01_10_211343_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('reply_count')->default(0);
+            $table->dateTimeTz('last_reply');
+            $table->string('last_replier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
