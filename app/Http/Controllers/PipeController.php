ebb8a254abd5d12388cb67cc2f6127aa5fa65f1d
Mark Gregan <mark@gregan.org>
2018-01-10 20:52:28 -0500

    Adding model, controller, and migration for Pipes

---
+++ app/Http/Controllers/PipeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pipe;
+use Illuminate\Http\Request;
+
+class PipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pipe  $pipe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pipe $pipe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pipe  $pipe
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pipe $pipe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pipe  $pipe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pipe $pipe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pipe  $pipe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pipe $pipe)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_01_10_211339_create_pipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('server');
+            $table->string('username');
+            $table->string('password');
+            $table->unsignedInteger('default_department_id')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pipes');
+    }
+}
