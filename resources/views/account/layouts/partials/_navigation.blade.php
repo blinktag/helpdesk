1cdb00320b7f23c24b36ec6ab12a8acb86bc8c54
Mark Gregan <mark@gregan.org>
2018-01-24 21:05:25 -0500

    Adding navigation helper

--- resources/views/account/layouts/partials/_navigation.blade.php
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -1,11 +1,11 @@
 <ul class="nav nav-pills nav-stacked">
-    <li>
+    <li class="{{ return_if(on_page('account'), 'active') }}">
         <a href="{{ route('account.index') }}">Account Overview</a>
     </li>
-    <li>
-        <a href="{{ route('account.password.index') }}">Change Password</a>
-    </li>
-    <li>
+    <li class="{{ return_if(on_page('account/profile'), 'active') }}">
         <a href="{{ route('account.profile.index') }}">Profile</a>
+    </li>
+    <li class="{{ return_if(on_page('account/password'), 'active') }}">
+        <a href="{{ route('account.password.index') }}">Change Password</a>
     </li>
 </ul>
