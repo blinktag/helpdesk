09ad57b42803088e3bf4f84724aee1546c1c4fc0
Mark Gregan <mark@gregan.org>
2018-01-26 21:04:38 -0500

    prevent password resets from logging in user who isn't activated

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +35,21 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse($response)
+    {
+        if (!$this->guard()->user()->activated) {
+            $this->guard()->logout();
+        }
+
+        return redirect($this->redirectPath())
+                            ->with('status', trans($response));
     }
 }
