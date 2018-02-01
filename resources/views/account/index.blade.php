fd3305a820f8e0f08f5e5c63487cae54cbda2aa1
Mark Gregan <mark@gregan.org>
2018-01-31 19:35:34 -0500

    Adding basic info to account page

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,5 +1,18 @@
 @extends('account.layouts.default')
 
 @section('account.content')
-account content
+
+<table class="table">
+    <tbody>
+        <tr>
+            <td width="33%">Name</td>
+            <td>{{ auth()->user()->name }}</td>
+        </tr>
+        <tr>
+            <td width="33%">Primary E-Mail</td>
+            <td>{{ auth()->user()->email }}</td>
+        </tr>
+    </tbody>
+</table>
+
 @endsection
