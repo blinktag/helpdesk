789d5668d7e24279fcf052a4a9d691338c1e8434
Mark Gregan <mark@gregan.org>
2018-02-01 21:00:02 -0500

    Moving ticket response HTML into a partial

--- resources/views/ticket/show.blade.php
+++ resources/views/ticket/show.blade.php
@@ -6,37 +6,8 @@
     <div class="row">
         <div class="col-md-12">
             @foreach($ticket->responses as $response)
-            <table class="table table-bordered">
-                <tbody>
-                    <tr>
-                        <td width="20%">
-                            <div class="text-center">
-                                <p>
-                                    <img src="{{ $response->user->getGravatarUrl() }}" class="rounded-circle" />
-                                </p>
-                                <p>
-                                {{ $response->user->name }}
-                                </p>
-                            </div>
-                        </td>
-                        <td valign="top">
-                            {!! nl2br(e($response->content)) !!}
-                        </td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="2">
-                            {{ $response->created_at->diffForHumans()}}
-
-                            <span class="float-right">
-                                {{ $response->from }}
-                            </span>
-                        </td>
-                    </tr>
-                </tfoot>
-                @endforeach
-            </table>
+                @include('ticket.partials._response')
+            @endforeach
         </div>
     </div>
 
