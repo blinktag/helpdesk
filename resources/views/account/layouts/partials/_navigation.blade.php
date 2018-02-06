a8850abc043cd79ec37b2f8744fbcebcbb897c70
Mark Gregan <mark@gregan.org>
2018-02-05 19:01:12 -0500

    Fixing active class in account navigation

--- resources/views/account/layouts/partials/_navigation.blade.php
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -1,5 +1,5 @@
 <div class="nav flex-column nav-pills" id="v-pills-tab" aria-orientation="vertical">
-  <a class="nav-link { return_if(on_page('account'), 'active') }}" id="v-pills-home-tab" href="{{ route('account.index') }}" aria-controls="v-pills-home" aria-selected="true">Account Overview</a>
-  <a class="nav-link { return_if(on_page('account/profile'), 'active') }}" id="v-pills-profile-tab" href="{{ route('account.profile.index') }}" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-  <a class="nav-link { return_if(on_page('account/password'), 'active') }}" id="v-pills-messages-tab" href="{{ route('account.password.index') }}" aria-controls="v-pills-messages" aria-selected="false">Change Password</a>
+  <a class="nav-link {{ return_if(on_page('account'), 'active') }}" id="v-pills-home-tab" href="{{ route('account.index') }}" aria-controls="v-pills-home" aria-selected="true">Account Overview</a>
+  <a class="nav-link {{ return_if(on_page('account/profile'), 'active') }}" id="v-pills-profile-tab" href="{{ route('account.profile.index') }}" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+  <a class="nav-link {{ return_if(on_page('account/password'), 'active') }}" id="v-pills-messages-tab" href="{{ route('account.password.index') }}" aria-controls="v-pills-messages" aria-selected="false">Change Password</a>
  </div>
