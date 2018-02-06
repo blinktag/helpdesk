ee88fb44c5e17b2fbc5d3eb33b58cf27bb992406
Mark Gregan <mark@gregan.org>
2018-02-05 19:14:29 -0500

    Disabling scout_driver in testing

--- database/migrations/2018_01_10_211343_create_tickets_table.php
+++ database/migrations/2018_01_10_211343_create_tickets_table.php
@@ -18,7 +18,7 @@ class CreateTicketsTable extends Migration
             $table->unsignedInteger('department_id')->index();
             $table->string('subject');
             $table->unsignedInteger('user_id')->index();
-            $table->enum('status', ['open','hold','closed']);
+            $table->enum('status', ['open','hold','closed'])->default('open');
             $table->unsignedInteger('reply_count')->default(0);
             $table->dateTimeTz('last_reply')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->string('last_replier');
